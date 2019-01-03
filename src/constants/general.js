507c1d017cfee28b80b30d5782e072169555ecea
David Ryan <davidryan1998@hotmail.com>
2019-01-03 11:42:47 +0700

    added notation for mercator, small and tiny Pythagorean commas

--- src/constants/general.js
+++ src/constants/general.js
@@ -10,6 +10,9 @@ var peoB = new Peo(243, 128)
 var peoOctave = new Peo(2)
 var peoSharp = new Peo({2:-11, 3:7})
 var peoPythag = new Peo({2:-19, 3:12})
+var peoMercator = new Peo({2:-84, 3:53})
+var peoSmall = new Peo({2:-1054, 3:665})
+var peoTiny = new Peo({2:-301994, 3:190537})
 var peoSyntonic = new Peo({2:4, 3:-4, 5:1})
 
 var result = {
@@ -34,6 +37,9 @@ var result = {
   PEO_OCTAVE: peoOctave,
   PEO_SHARP: peoSharp,
   PEO_PYTHAG: peoPythag,
+  PEO_MERCATOR: peoMercator,
+  PEO_SMALL: peoSmall,
+  PEO_TINY: peoTiny,
   PEO_SYNTONIC: peoSyntonic,
 
   NAME_2_OCTAVE: "o",
@@ -41,6 +47,12 @@ var result = {
   NAME_3_FLAT: "b",
   NAME_3_PYTHAG_COMMA_ADD: "p",
   NAME_3_PYTHAG_COMMA_REMOVE: "d",
+  NAME_3_MERCATOR_COMMA_ADD: "m",
+  NAME_3_MERCATOR_COMMA_REMOVE: "w",
+  NAME_3_SMALL_COMMA_ADD: "s",
+  NAME_3_SMALL_COMMA_REMOVE: "r",
+  NAME_3_TINY_COMMA_ADD: "t",
+  NAME_3_TINY_COMMA_REMOVE: "y",
   NAME_5_SYNTONIC_COMMA_ADD: "'",
   NAME_5_SYNTONIC_COMMA_REMOVE: ".",
 
