93ab9531a7c94a802002d961a8749f30aa6e81ad
David Ryan <davidryan1998@hotmail.com>
2019-12-03 11:54:11 +0700

    Started new feature

---
+++ src/JPoint.js
@@ -0,0 +1,4 @@
+function JPoint() {
+}
+
+module.exports = JPoint;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 var JInterval = require('./JInterval');
+var JPoint = require('./JPoint');
 var getComma = require('./api/class/getComma');
 var getCommaAlgs = require('./api/class/getCommaAlgs');
 
 module.exports = {
   jinterval: JInterval,
   JInterval: JInterval,
+  jpoint: JPoint,
+  JPoint: JPoint,
   getComma: getComma,
   getCommaAlgs: getCommaAlgs
 };
